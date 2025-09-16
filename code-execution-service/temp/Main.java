e01f47282e0d0fa17829ebb95e5a770b9bc5783c
Neelam sruthi <sruthineelam118@gmail.com>
2025-09-16 09:36:32 +0530

    updated docker code

--- code-execution-service/temp/Main.java
+++ code-execution-service/temp/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Your code here
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x%2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
     }
 }
